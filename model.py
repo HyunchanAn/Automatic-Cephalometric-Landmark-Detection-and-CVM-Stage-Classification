9aa3eaf0ba10f4ab0a211dd8856f8e313e64369e
HyunchanAn <chemahc94@gmail.com>
2025-09-26 09:04:45 +0900

    error fix
    아키텍처와 해상도 불일치 수정, 학습 로그 정리

--- model.py
+++ model.py
@@ -8,32 +8,23 @@ class UNetHeatmapModel(nn.Module):
     def __init__(self, num_landmarks=NUM_LANDMARKS, pretrained=True):
         super(UNetHeatmapModel, self).__init__()
         
-        # Load a pre-trained ResNet-50 model
         resnet = models.resnet50(weights=models.ResNet50_Weights.DEFAULT if pretrained else None)
 
         # --- Encoder (ResNet-50 Backbone) ---
         self.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
-        self.layer1 = resnet.layer1 # Output: 256 channels
-        self.layer2 = resnet.layer2 # Output: 512 channels
-        self.layer3 = resnet.layer3 # Output: 1024 channels
-        self.layer4 = resnet.layer4 # Output: 2048 channels
+        self.layer1 = resnet.layer1 # Output: 256 channels, 1/4 size
+        self.layer2 = resnet.layer2 # Output: 512 channels, 1/8 size
+        self.layer3 = resnet.layer3 # Output: 1024 channels, 1/16 size
+        self.layer4 = resnet.layer4 # Output: 2048 channels, 1/32 size
 
         # --- Decoder (Upsampling with Skip Connections) ---
-        # Upsampling from layer4 to layer3's size
         self.up_conv4 = self._upsample_block(2048, 1024)
-        # Upsampling from (up_conv4 + layer3) to layer2's size
         self.up_conv3 = self._upsample_block(1024 + 1024, 512)
-        # Upsampling from (up_conv3 + layer2) to layer1's size
         self.up_conv2 = self._upsample_block(512 + 512, 256)
-        # Upsampling from (up_conv2 + layer1) to layer0's size
-        self.up_conv1 = self._upsample_block(256 + 256, 256)
-        # Final upsampling to heatmap size
-        self.up_conv0 = self._upsample_block(256, 128)
 
         # Final layer to produce the heatmaps
-        self.final_layer = nn.Conv2d(128, num_landmarks, kernel_size=1, stride=1, padding=0)
+        self.final_layer = nn.Conv2d(256, num_landmarks, kernel_size=1, stride=1, padding=0)
 
-        # Freeze backbone by default
         self.freeze_backbone()
 
     def _upsample_block(self, in_channels, out_channels):
@@ -45,21 +36,18 @@ class UNetHeatmapModel(nn.Module):
 
     def forward(self, x):
         # --- Encoder --- 
-        x0 = self.layer0(x)      # Size: 1/4, Channels: 64
-        x1 = self.layer1(x0)     # Size: 1/4, Channels: 256
-        x2 = self.layer2(x1)     # Size: 1/8, Channels: 512
-        x3 = self.layer3(x2)     # Size: 1/16, Channels: 1024
-        x4 = self.layer4(x3)     # Size: 1/32, Channels: 2048
+        x0 = self.layer0(x)
+        x1 = self.layer1(x0)
+        x2 = self.layer2(x1)
+        x3 = self.layer3(x2)
+        x4 = self.layer4(x3)
 
         # --- Decoder with Skip Connections ---
         u4 = self.up_conv4(x4)
         u3 = self.up_conv3(torch.cat([u4, x3], 1))
         u2 = self.up_conv2(torch.cat([u3, x2], 1))
-        u1 = self.up_conv1(torch.cat([u2, x1], 1))
-        u0 = self.up_conv0(u1)
 
-        # --- Final Output ---
-        heatmaps = self.final_layer(u0)
+        heatmaps = self.final_layer(u2)
         
         return heatmaps
 
